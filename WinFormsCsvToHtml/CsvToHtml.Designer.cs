acea3d6b5d4312c5e828178bac681f236496231c
masi23 <mateuszzwierzchowski19@gmail.com>
2024-10-24 19:14:30 +0200

    Dodaj pliki projektów.

---
+++ WinFormsCsvToHtml/CsvToHtml.Designer.cs
@@ -0,0 +1,217 @@
+﻿
+namespace WinFormsCsvToHtml
+{
+    partial class CsvToHtml
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.saveChangesButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Wejście";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 53);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(187, 23);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(199, 53);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Wybierz plik";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(70, 35);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(302, 100);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(429, 35);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(302, 100);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(6, 53);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(187, 23);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(199, 53);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Wybierz plik";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Wyjście";
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.LightGreen;
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button3.Location = new System.Drawing.Point(582, 407);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(149, 31);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Konwertuj";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(76, 180);
+            this.textBox3.MaximumSize = new System.Drawing.Size(302, 180);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(287, 180);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3Changed);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(76, 162);
+            this.label3.MaximumSize = new System.Drawing.Size(300, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Plik źródłowy:";
+            // 
+            // saveChangesButton
+            // 
+            this.saveChangesButton.Location = new System.Drawing.Point(243, 366);
+            this.saveChangesButton.Name = "saveChangesButton";
+            this.saveChangesButton.Size = new System.Drawing.Size(120, 23);
+            this.saveChangesButton.TabIndex = 8;
+            this.saveChangesButton.Text = "Zapisz zmiany";
+            this.saveChangesButton.UseVisualStyleBackColor = true;
+            this.saveChangesButton.Visible = false;
+            this.saveChangesButton.Click += new System.EventHandler(this.saveChangesButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.saveChangesButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "CSV to HTML konwerter";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button saveChangesButton;
+    }
+}
+
---
+++ WinFormsCsvToHtml/CsvToHtml.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace WinFormsCsvToHtml
+{
+
+    public partial class CsvToHtml : Form
+    {
+
+        string srcPath = "";                    //source file (.CSV) directory
+        string destPath = "";                   //destination file (.HTML) directory
+        string lastOpenedDirectory = @"C:\";    //holds recently opened folder directory
+        public CsvToHtml()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Choose source file with OpenFileDialog
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.Title = "Wybierz plik";
+            openFileDialog.Filter = "Pliki CSV (*.csv)|*.csv";
+            openFileDialog.InitialDirectory = lastOpenedDirectory;
+
+            if(openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                //Read selected CSV file and display its content with textBox
+                srcPath = openFileDialog.FileName;
+                lastOpenedDirectory = Path.GetDirectoryName(srcPath);
+                label3.Text = "Plik źródłowy: " + Path.GetFileName(srcPath);
+                textBox1.Text = srcPath;
+                string csvText = File.ReadAllText(srcPath);
+                textBox3.Text = csvText;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //choose destination file with SaveFileDialog
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            saveFileDialog.Title = "Wybierz plik";
+            saveFileDialog.Filter = "Pliki HTML (*.html)|*.html";
+            saveFileDialog.InitialDirectory = lastOpenedDirectory;
+
+            //show info dialog
+            if(saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                destPath = saveFileDialog.FileName;
+                textBox2.Text = destPath;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(srcPath != "" && destPath != "")
+            {
+                //use HTML format to create table and fill it with data
+                bool firstLine = true;
+                File.WriteAllText(destPath, "<html><head><style>" +
+                    "table, tr, td{ border: 1px solid lightgray; border-collapse: collapse;} " +
+                    "tr:nth-child(even) {background: lightgray;}</style></head><body><table>");
+
+                
+                string[] lines = File.ReadAllLines(srcPath);
+                foreach(var line in lines)
+                {
+                    File.AppendAllText(destPath, "<tr>");
+                    string[] values = line.Split(",");
+                    foreach (var value in values)
+                    {
+                        //display first line of data in special (header) table row
+                        if (firstLine)
+                        {
+                            File.AppendAllText(destPath, "<th>" + value + "</th>");
+                        }
+                        else
+                        {
+                            File.AppendAllText(destPath, "<td>" + value + "</td>");
+                        }
+                    }
+                    if (firstLine)
+                    {
+                        firstLine = false;
+                    }
+                    File.AppendAllText(destPath, "</tr>");
+                }
+                //finish file in proper HTML style
+                File.AppendAllText(destPath, "</table></body></html>");
+
+                //open newly created/altered HTML file in default web browser
+                try
+                {
+                    Process.Start(new ProcessStartInfo
+                    {
+                        FileName = destPath,
+                        UseShellExecute = true
+                    });
+                } catch(Exception ex)
+                {
+                    MessageBox.Show("Wystąpił błąd: " + ex.Message);
+                }
+            } else
+            {
+                MessageBox.Show("Nie wybrano ścieżki dla pliku.", "Uwaga", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void textBox3Changed(object sender, EventArgs e)
+        {
+            saveChangesButton.Visible = true;
+        }
+
+        //save changes made in CSV-displaying textBox with SaveFileDialog
+        private void saveChangesButton_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Title = "Wybierz plik";
+            sfd.Filter = "Pliki CSV (*.csv)|*.csv";
+            if(sfd.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(srcPath, textBox3.Text);
+                MessageBox.Show("Plik został zapisany pomyślnie", "Sukces", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
